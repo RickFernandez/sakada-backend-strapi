72cc40fa328f57eda82fbdedd326b2a5197a5ed9
Henrique Fernandez <rickfernandez03@gmail.com>
2024-09-25 19:26:27 -0300

    feat: adding project-filter collection

---
+++ src/api/project-filter/controllers/project-filter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-filter controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::project-filter.project-filter');
---
+++ src/api/project-filter/services/project-filter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-filter service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::project-filter.project-filter');
